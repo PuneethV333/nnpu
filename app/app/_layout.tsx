ef39d22003d7e2a4b6d0f32da77827ebcba7225f
Puneeth V <puneethvijayakumar333@gmail.com>
2026-07-16 15:26:34 +0530

    add:ui-start

---
+++ app/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { AuthProvider } from "@/context/AuthContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 30_000,
+    },
+  },
+});
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        {/* <AuthProvider> */}
+          <StatusBar style="auto" />
+          <Slot />
+        {/* </AuthProvider> */}
+      </QueryClientProvider>
+    </SafeAreaProvider>
+  );
+}
